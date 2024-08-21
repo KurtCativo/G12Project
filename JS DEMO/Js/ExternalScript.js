de7b837af2a9aa8e82a04bf3c29f636d94b80650
KurtCativo <155300613+KurtCativo@users.noreply.github.com>
2024-08-21 03:49:34 -0230

    Add files via upload

---
+++ JS DEMO/Js/ExternalScript.js
@@ -0,0 +1,9 @@
+
+function myGreeting(){
+    alert("Hello JavaScript!");
+}
+function showAlert(){
+    alert("Pease Click OK to continue.");
+    alert("Pease Click OK again");
+    alert("Click OK for the last time to continue");
+}
