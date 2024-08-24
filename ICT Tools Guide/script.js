0c1524e71887ce67c677f3299de34225db5bc492
KurtCativo <155300613+KurtCativo@users.noreply.github.com>
2024-08-24 18:46:06 +0800

    Delete ICT Tools Guide directory

--- ICT Tools Guide/script.js
+++
@@ -1,8 +0,0 @@
-// Add event listener to navigation links
-document.querySelectorAll('nav a').forEach(function(link) {
-	link.addEventListener('click', function(event) {
-		event.preventDefault();
-		document.querySelector(link.getAttribute('href')).scrollIntoView();
-	});
-});
-
